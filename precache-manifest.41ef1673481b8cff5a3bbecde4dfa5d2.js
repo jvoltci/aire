d228fc63de60d8232d4682a97ff8672c35a4e882
jvoltci <jvoltci@gmail.com>
2019-09-05 08:12:32 +0530

    Updates

---
+++ precache-manifest.41ef1673481b8cff5a3bbecde4dfa5d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1728730e988b34c0d8d50dbe417d739e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa33fa1687d99897aa9d",
+    "url": "/static/css/main.714e92f0.chunk.css"
+  },
+  {
+    "revision": "e86c80bdc6861965b23a",
+    "url": "/static/js/2.b38c676b.chunk.js"
+  },
+  {
+    "revision": "fa33fa1687d99897aa9d",
+    "url": "/static/js/main.d0b80a39.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
